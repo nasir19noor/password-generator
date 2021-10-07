7d63d549a4c10ea21878ad794f95a891013a2d9f
Nasir Nooruddin <nasir@nasir.id>
2021-10-07 23:11:41 +0700

    Revision 1

--- password-generator.py
+++ password-generator.py
@@ -15,35 +15,24 @@ password_total_1 = ""
 total_numbers = 0
 
 for i in range(0, number_letters):
-  random_number = random.randint(0, 52)
+  random_number = random.randint(0, 51)
   password_letters += letters[random_number]
   print(letters[random_number])
 total_numbers += number_letters
-print(password_letters)
-print(total_numbers)
 
 for i in range(0, number_symbols):
   random_number = random.randint(0, 11)
   password_symbols += symbols[random_number]
   print(symbols[random_number])
 total_numbers += number_symbols
-print(password_symbols)
-print(total_numbers)
 
 for i in range(0, number_numbers):
   random_number = random.randint(0, 9)
   password_numbers += numbers[random_number]
   print(numbers[random_number])
 total_numbers += number_numbers
-print(password_numbers)
-print(total_numbers)
 
-password_total_1 = password_letters + password_symbols + password_numbers
-print(password_total_1)
-
+password_total_1 = password_letters + password_symbols + password_number
 password_total_2 = list(password_total_1)
-print(password_total_2)
 random.shuffle(password_total_2)
-
 password_total_3 = ''.join(password_total_2)
-print(password_total_3)
